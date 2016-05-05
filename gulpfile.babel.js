459809408ab2c54a4555bbeff1137058f4d51359
techstalgist <mikko.i.ruuskanen@gmail.com>
2016-05-05 19:56:15 +0300

    Added layout

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,7 +10,8 @@ import babelify from 'babelify';
 
 const sync = browserSync.create();
 
-// Input file.
+// Input file is src/app.js
+//watchify: Update any source file and your browserify bundle will be recompiled on the spot.
 watchify.args.debug = true;
 var bundler = browserify('src/app.js', watchify.args);
 
@@ -18,6 +19,9 @@ var bundler = browserify('src/app.js', watchify.args);
 bundler.transform(babelify.configure({
   sourceMapRelative: 'src'
 }));
+
+//bundle.js.map (source map?) is needed so that in debugging you see which src file causes the error.
+// the alternative is to have a huge bundle.js and try to debug that file -> not good.
 
 // On updates recompile
 bundler.on('update', bundle);
@@ -38,15 +42,20 @@ gulp.task('default', ['transpile']);
 
 gulp.task('transpile', ['lint'], () => bundle());
 
+// lint helps in finding bugs in compile stage, instead of finding them live on the browser.
+// most useful when npm start is running so you see errors right after edits
 gulp.task('lint', () => {
     return gulp.src(['src/**/*.js', 'gulpfile.babel.js'])
       .pipe(eslint())
       .pipe(eslint.format())
 });
 
+//use proxy if there is another server (such as a Rails server running at localhost:3000)
+//apparently BrowserSync will run on another port, like 3001.
 gulp.task('serve', ['transpile'], () => sync.init({ proxy: 'http://localhost:3000'}))
 gulp.task('js-watch', ['transpile'], () => sync.reload());
 
+//which files to watch?
 gulp.task('watch', ['serve'], () => {
   gulp.watch('src/**/*', ['js-watch'])
   gulp.watch('public/assets/style.css', sync.reload)
--- public/assets/bundle.js
+++ public/assets/bundle.js
@@ -19679,15 +19679,15 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _features = require('./components/features');
+var _layout = require('./components/layout');
 
-var _features2 = _interopRequireDefault(_features);
+var _layout2 = _interopRequireDefault(_layout);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom2.default.render(_react2.default.createElement(_features2.default, null), document.getElementById('features-app'));
+_reactDom2.default.render(_react2.default.createElement(_layout2.default, null), document.getElementById('features-app'));
 
-},{"./components/features":174,"react":169,"react-dom":3}],171:[function(require,module,exports){
+},{"./components/layout":175,"react":169,"react-dom":3}],171:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -20114,6 +20114,10 @@ var Features = function (_React$Component) {
 			supporting_features: []
 		};
 
+		_this.handleFeatureUpdate = _this.handleFeatureUpdate.bind(_this);
+		_this.handleFeatureDelete = _this.handleFeatureDelete.bind(_this);
+		_this.handleFeatureSubmit = _this.handleFeatureSubmit.bind(_this);
+
 		return _this;
 	}
 
@@ -20262,14 +20266,14 @@ var Features = function (_React$Component) {
 					_react2.default.createElement(
 						'div',
 						{ className: 'col-md-12' },
-						_react2.default.createElement(_featureList2.default, { features: this.state.core_features, type: 'Core features', onFeatureUpdate: this.handleFeatureUpdate.bind(this), onFeatureDelete: this.handleFeatureDelete.bind(this) }),
-						_react2.default.createElement(_featureList2.default, { features: this.state.supporting_features, type: 'Supporting features', onFeatureUpdate: this.handleFeatureUpdate.bind(this), onFeatureDelete: this.handleFeatureDelete.bind(this) })
+						_react2.default.createElement(_featureList2.default, { features: this.state.core_features, type: 'Core features', onFeatureUpdate: this.handleFeatureUpdate, onFeatureDelete: this.handleFeatureDelete }),
+						_react2.default.createElement(_featureList2.default, { features: this.state.supporting_features, type: 'Supporting features', onFeatureUpdate: this.handleFeatureUpdate, onFeatureDelete: this.handleFeatureDelete })
 					)
 				),
 				_react2.default.createElement(
 					'div',
 					{ className: 'col-md-4' },
-					_react2.default.createElement(_featureForm2.default, { onFeatureSubmit: this.handleFeatureSubmit.bind(this) })
+					_react2.default.createElement(_featureForm2.default, { onFeatureSubmit: this.handleFeatureSubmit })
 				)
 			);
 		}
@@ -20280,5 +20284,148 @@ var Features = function (_React$Component) {
 
 exports.default = Features;
 
-},{"./feature-form":171,"./feature-list":172,"react":169,"react-addons-update":2}]},{},[170])
+},{"./feature-form":171,"./feature-list":172,"react":169,"react-addons-update":2}],175:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _features = require('./features');
+
+var _features2 = _interopRequireDefault(_features);
+
+var _menu = require('./menu');
+
+var _menu2 = _interopRequireDefault(_menu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Layout = function (_React$Component) {
+  _inherits(Layout, _React$Component);
+
+  function Layout() {
+    _classCallCheck(this, Layout);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Layout).apply(this, arguments));
+  }
+
+  _createClass(Layout, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(_menu2.default, null),
+        _react2.default.createElement(_features2.default, null)
+      );
+    }
+  }]);
+
+  return Layout;
+}(_react2.default.Component);
+
+exports.default = Layout;
+
+},{"./features":174,"./menu":176,"react":169}],176:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Menu = function (_React$Component) {
+  _inherits(Menu, _React$Component);
+
+  function Menu() {
+    _classCallCheck(this, Menu);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(Menu).apply(this, arguments));
+  }
+
+  _createClass(Menu, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement(
+        "div",
+        { classNameName: "row" },
+        _react2.default.createElement(
+          "nav",
+          { className: "navbar navbar-default" },
+          _react2.default.createElement(
+            "div",
+            { className: "navbar-header" },
+            _react2.default.createElement(
+              "a",
+              { className: "navbar-brand", href: "#" },
+              "Features App"
+            )
+          ),
+          _react2.default.createElement(
+            "div",
+            { className: "container-fluid" },
+            _react2.default.createElement(
+              "div",
+              { className: "collapse navbar-collapse" },
+              _react2.default.createElement(
+                "ul",
+                { className: "nav navbar-nav" },
+                _react2.default.createElement(
+                  "li",
+                  null,
+                  _react2.default.createElement(
+                    "a",
+                    { href: "#" },
+                    "Intro page"
+                  )
+                ),
+                _react2.default.createElement(
+                  "li",
+                  { className: "active" },
+                  _react2.default.createElement(
+                    "a",
+                    { href: "#" },
+                    "Board"
+                  )
+                )
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+
+  return Menu;
+}(_react2.default.Component);
+
+exports.default = Menu;
+
+},{"react":169}]},{},[170])
 //# sourceMappingURL=bundle.js.map
